5dad5ebb1b42159dd1ccb82fd175442da30edcc3
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-01-24 14:32:54 -0500

    phase 0

--- practice-for-week-15-react-refactoring-class-components-long-practice-main/src/App.js
+++ practice-for-week-15-react-refactoring-class-components-long-practice-main/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Clock, { ClockToggle } from './components/Clock';
 import Folder from './components/Folder';
 import Weather from './components/Weather';
@@ -21,27 +21,49 @@ const folders = [
   { title: 'three', content: 'Third folder here' }
 ];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showClock: true
-    };
-  }
-  
-  toggleClock = () => this.setState({ showClock: !this.state.showClock });
-  
-  render () {
-    return (
-      <div className="widgets">
-        <Folder folders={folders} />
-        <Weather />
-        <ClockToggle toggleClock={this.toggleClock} />
-        {this.state.showClock && <Clock />}
-        <AutoComplete names={names} />
-      </div>
-    );
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showClock: true
+//     };
+  // }
+
+const App = function(props) {
+ 
+ const [showClock, setShowClock] = useState(true) 
+//   toggleClock = () => this.setState({ showClock: !this.state.showClock });
+
+const toggleClock = () => setShowClock ({
+  showClock: !showClock
+})
+
+//   render () {
+//     return (
+//       <div className="widgets">
+//         <Folder folders={folders} />
+//         <Weather />
+//         <ClockToggle toggleClock={this.toggleClock} />
+//         {this.state.showClock && <Clock />}
+//         <AutoComplete names={names} />
+//       </div>
+//     );
+//   }
+// }
+
+  return (
+    <div className="widgets">
+      <Folder folders={folders} />
+      <Weather />
+      <ClockToggle toggleClock />
+      {showClock && <Clock />}
+      <AutoComplete names={names} />
+    </div>
+  );
 }
+
+
+  
+
 
 export default App;
